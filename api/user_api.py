934023aa90a8bffb7f19d1f4ca6a13dec2dcacd4
bf01863644 <bsd_unix@naver.com>
2025-01-22 10:53:41 +0900

    style: localhost to 127.0.0.1

--- api/user_api.py
+++ api/user_api.py
@@ -15,7 +15,7 @@ router = APIRouter(prefix='/auth', tags=['user'])
 user_logger = create_logger('user-log')
 auth_handler = AuthHandler()
 redis_client = Redis(
-    host=os.getenv('REDIS_HOST', 'localhost'),
+    host=os.getenv('REDIS_HOST', '127.0.0.1'),
     port=int(os.getenv('REDIS_PORT', 6379)),
     db=0,
     decode_responses=True
