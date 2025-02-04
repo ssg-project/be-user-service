688a1b6f8e917a04aa913afdae0a62e75c18d4d2
myeongsoyi <myeongsoyi@gmail.com>
2025-02-04 13:48:40 +0900

    feat: health check 추가

--- main.py
+++ main.py
@@ -6,26 +6,11 @@ import uvicorn
 
 app = FastAPI()
 
-# # middleware 설정
-# app.add_middleware( # cors middleware
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# app.add_middleware( # session middleware
-#     SessionMiddleware,
-#     secret_key="your_secret_key", # env 파일로 옮길 예정
-# )
-
-# router 설정
 app.include_router(user_router, prefix="/api/v1", tags=["user"])
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello World"}
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8001, reload=True)
