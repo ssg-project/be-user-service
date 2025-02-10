84c878c88f624c11d695c24b6a622b0ad817a31a
myeongsoyi <myeongsoyi@gmail.com>
2025-02-10 14:56:22 +0900

    feat: 환경변수 호출 변경

--- utils/database.py
+++ utils/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-DATABASE_URL = f"mysql+pymysql://{os.environ['DB_USER']}:{os.environ['DB_PASS']}@{os.environ['DB_HOST']}/{os.environ['DB_NAME']}"
+DATABASE_URL = (
+    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}"
+    f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+)
 
 engine = create_engine(
     DATABASE_URL, 
