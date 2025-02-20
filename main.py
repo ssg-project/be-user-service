4094a55339bf887a6f077b121863e4fa80d0a10a
sh167603 <soohwan06@gmail.com>
2025-02-20 11:56:45 +0900

    main requirements 수정

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from api.user_api import router as user_router
+from prometheus_fastapi_instrumentator import Instrumentator
 import uvicorn
 
 app = FastAPI()
@@ -9,6 +10,8 @@ app.include_router(user_router, prefix="/api/v1", tags=["user"])
 @app.get("/health")
 async def health_check():
     return {"status": "ok"}
+
+Instrumentator().instrument(app).expose(app)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8001, reload=True)
