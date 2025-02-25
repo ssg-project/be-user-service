7de6716e76472429e3ad9f458dc0b3f7734809f4
sh167603 <soohwan06@gmail.com>
2025-02-25 11:18:32 +0900

    주석 제거, .env 파일 생성

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from api.user_api import router as user_router
-# from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_fastapi_instrumentator import Instrumentator
 import uvicorn
 
 app = FastAPI()
@@ -11,7 +11,7 @@ app.include_router(user_router, prefix="/api/v1", tags=["user"])
 async def health_check():
     return {"status": "ok"}
 
-# Instrumentator().instrument(app).expose(app)
+Instrumentator().instrument(app).expose(app)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8001, reload=True)
