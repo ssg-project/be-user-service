183424f591b1c2bca652386a81291d04b4f167f7
simsangho6448 <mjj00travel@gmail.com>
2025-02-28 12:02:07 +0900

    feat: pod,node log 추가

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ from api.user_api import router as user_router
 from prometheus_fastapi_instrumentator import Instrumentator
 import uvicorn
 import logging
+import os
 
-# 로깅 설정
-logging. basicConfig(
+# Kubernetes 환경에서 파드 및 노드 정보 가져오기
+pod_name = os.environ.get("POD_NAME", "unknown-pod")
+node_name = os.environ.get("NODE_NAME", "unknown-node")
+
+
+#로깅 설정
+logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - ticketing-service - %(name)s - %(levelname)s - %(message)s",
+    format=(
+        "%(asctime)s - user-service - %(name)s - %(levelname)s - %(message)s"
+        "{pod: %(pod_name)s, node: %(node_name)s}"
+    ),
     handlers=[logging.StreamHandler()]
 )
+
 
 app = FastAPI()
 
